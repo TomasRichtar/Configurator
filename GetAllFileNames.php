5474e887c784d4530570cb606c1a2ca45f70d97e
TomasRichtar <124818613+TomasRichtar@users.noreply.github.com>
2025-03-31 09:34:16 +0200

    Update GetAllFileNames.php

--- GetAllFileNames.php
+++ GetAllFileNames.php
@@ -1,31 +1,19 @@
 <?php
-​
 // Allow from any origin
 if (isset($_SERVER['HTTP_ORIGIN'])) {
-    // Decide if the origin in $_SERVER['HTTP_ORIGIN'] is one
-    // you want to allow, and if so:
     header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
     header('Access-Control-Allow-Credentials: true');
     header('Access-Control-Max-Age: 86400');    // cache for 1 day
 }
-​
-$ftp_username = "tastyair.cz";
-$ftp_userpass = "3uqUrQ8wdDGT";
-$ftp_server = "tastyair.cz";
-$ftp_dir = htmlspecialchars($_POST["ftpdir"]);
-​
-$ftp_connection = ftp_connect($ftp_server);
- 
-if($ftp_connection) {
-    $login = ftp_login($ftp_connection, $ftp_username, $ftp_userpass);
-     
-    if($login) {
-        $file_list = ftp_nlist($ftp_connection, $ftp_dir);
-        sort($file_list);
-        foreach($file_list as $key=>$dat) {
-            echo $dat."\n";
-       }
+
+// Directory where your files are located
+$dir = "."; // Current directory
+
+$file_list = scandir($dir);
+sort($file_list);
+foreach($file_list as $file) {
+    if ($file !== '.' && $file !== '..') {
+        echo $file . "\n";
     }
-    ftp_close($ftp_connection);
 }
-?>	
+?>
